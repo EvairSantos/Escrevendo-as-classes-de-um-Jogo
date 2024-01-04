204549cb7ce8c47db8e6694a7a5ffc84c51d0cdb
Evair Santos de Andrade <108219985+EvairSantos@users.noreply.github.com>
2024-01-04 14:18:00 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from hero import Heroi
+
+heroi1 = Heroi("Gandalf", 1000, "mago")
+heroi2 = Heroi("Aragorn", 35, "guerreiro")
+heroi3 = Heroi("Bruce Lee", 80, "monge")
+heroi4 = Heroi("Hanzo", 25, "ninja")
+
+heroi1.atacar()
+heroi2.atacar()
+heroi3.atacar()
+heroi4.atacar()
